9c6a2c47d88e2c224a01dcff152c3f31b1cc1a6d
Jason <jason784@gmail.com>
2017-03-27 19:34:01 -0400

    Safe images

---
+++ app/components/safe-image.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'img',
+  attributeBindings: ['src'],
+  classNameBindings: ['isBroken:broken'],
+
+  isBroken: false,
+
+  didInsertElement: function() {
+    this.$().on('error', function() {
+      this.set('isBroken', true);
+      console.log('alt', this.alt)
+      console.log(this)
+      this.$().attr('src', this.alt);
+    }.bind(this));
+  },
+
+  willDestroyElement: function(){
+    this.$().off();
+  }
+});
